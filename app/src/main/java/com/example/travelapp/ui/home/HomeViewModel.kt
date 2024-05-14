9a31d7980e64e732e670db1a26770bc19dbe8bb7
chean5o <oce20010605@gmail.com>
2024-05-15 02:10:57 +0900

    커밋 확인

--- app/src/main/java/com/example/travelapp/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/travelapp/ui/home/HomeViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "홈"
+        value = "홈 입니다용"
     }
     val text: LiveData<String> = _text
 }
