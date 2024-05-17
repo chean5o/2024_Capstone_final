7563121362a06940fe718659df45f18db8819e70
chean5o <oce20010605@gmail.com>
2024-05-18 00:54:42 +0900

    map 화면 검색기능 및 카테고리별 마커 분류 노드 서버 연결

--- app/src/main/java/com/example/travelapp/ui/makecourse/Question2.kt
+++ app/src/main/java/com/example/travelapp/ui/makecourse/Question2.kt
@@ -32,12 +32,12 @@ class Question2 : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        // Retrofit 인스턴스 생성 및 초기화
+// Retrofit 인스턴스 생성 및 초기화
         val retrofit = Retrofit.Builder()
             .baseUrl("http://10.0.2.2:5000/") // 본인의 서버 URL로 변경하세요
             .addConverterFactory(GsonConverterFactory.create())
             .build()
+
 
         voteService = retrofit.create(VoteService::class.java)
 
