40363d216407ea87dd804f4a20ef92e023a38a4c
chean5o <oce20010605@gmail.com>
2024-05-18 22:17:29 +0900

    map 화면 검색시 여러장소_ver 화면 생성 및 받아오는 데이터 형식 list로 변환 / 필요없는 주석 삭제

--- app/src/main/java/com/example/travelapp/PlaceService.kt
+++ app/src/main/java/com/example/travelapp/PlaceService.kt
@@ -6,7 +6,7 @@ import com.example.travelapp.ui.map.Location
 
 interface PlaceService {
     @GET("/find_map/{name}")
-    fun getPlaceDetails(@Path("name") name: String): Call<Location>
+    fun getPlaceDetails(@Path("name") name: String): Call<List<Location>>
 
     @GET("/places/{category}")
     fun getPlaces(@Path("category") category: Int): Call<List<Location>> // Location은 앞서 정의한 데이터 모델입니다.
