9bbb0f02916423e7ad1322a9c545e935a161e03b
subb1n <haphpy@naver.com>
2024-05-19 07:25:55 +0900

    home image

--- app/src/main/java/com/example/travelapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/travelapp/ui/home/HomeFragment.kt
@@ -1,15 +1,24 @@
 package com.example.travelapp.ui.home
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.HorizontalScrollView
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.travelapp.PlaceService
 import com.example.travelapp.R
 import com.example.travelapp.databinding.FragmentHomeBinding
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class HomeFragment : Fragment() {
 
@@ -18,6 +27,17 @@ class HomeFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    object RetrofitClient {
+        private const val BASE_URL = "http://192.168.50.34:3000/"
+
+        val instance: PlaceService by lazy {
+            Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(PlaceService::class.java)
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -31,21 +51,29 @@ class HomeFragment : Fragment() {
         val root: View = binding.root
 
         val button1: TextView = root.findViewById(R.id.Button1)
-        val button2: TextView = root.findViewById(R.id.Button2)
-        val horizontalScrollView: HorizontalScrollView = root.findViewById(R.id.horizontalScrollView)
 
-        button1.setOnClickListener {
-            horizontalScrollView.post {
-                horizontalScrollView.fullScroll(View.FOCUS_LEFT)
-            }
-        }
 
-        button2.setOnClickListener {
-            horizontalScrollView.post {
-                horizontalScrollView.fullScroll(View.FOCUS_RIGHT)
-            }
-        }
+        val imageView = binding.imageView
+        loadImage(imageView)
+
         return root
+    }
+    private fun loadImage(imageView: ImageView) {
+        RetrofitClient.instance.getImageUrl().enqueue(object : Callback<ResponseBody> {
+            override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
+                if (response.isSuccessful) {
+                    response.body()?.byteStream()?.let { inputStream ->
+                        val bitmap = BitmapFactory.decodeStream(inputStream)
+                        imageView.setImageBitmap(bitmap) // 이미지 뷰에 비트맵 설정
+                    }
+                } else {
+                    Log.e("API Error", "Response not successful")
+                }
+            }
+            override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                Log.e("API Error", "Network error", t)
+            }
+        })
     }
 
     override fun onDestroyView() {
